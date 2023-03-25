826b91d7d4877eb807f6aa4fc91ae60f99210d81
seven <8536397+seven001@user.noreply.gitee.com>
2023-03-25 10:25:33 +0800

    feat:添加art-provider

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-import "whatwg-fetch";
-import { Art } from "./lib/art";
-import { makeFetch, useFetch, makeSubmit, useSubmit } from "./lib/fetch";
-import { useAutoRun } from "./lib/hooks";
-import resso from "./lib/obs/resso";
+import 'whatwg-fetch'
+import { Art } from './lib/art'
+import { makeFetch, useFetch, makeSubmit, useSubmit } from './lib/fetch'
+import { ArtProvider } from './lib/art-provider'
+import { useAutoRun } from './lib/hooks'
+import resso from './lib/obs/resso'
 
-export * from "./lib/model";
-export { Art, resso, makeFetch, makeSubmit, useFetch, useSubmit, useAutoRun };
+export * from './lib/model'
+export {
+  Art,
+  resso,
+  makeFetch,
+  makeSubmit,
+  useFetch,
+  useSubmit,
+  useAutoRun,
+  ArtProvider
+}
